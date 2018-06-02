e9874eb50498dc3f2b280db90cd3e1916a2bee2f
Danaru <danaru@tuta.io>
2018-06-02 17:45:05 +0200

    v0.2 Prise en charge des API Gmail Adding Gmail APIs

--- UnseenMail.py
+++ UnseenMail.py
@@ -1,15 +1,19 @@
-import imaplib, os
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+import imaplib
+import os
 import configparser
 
 dirname = os.path.split(os.path.abspath(__file__))[0]
-
 accounts = configparser.ConfigParser()
 accounts.read(os.path.abspath(dirname + '/accounts.ini'))
-
 strFormatted = ""
 
 
-def check_unread(imap_account):
+def check_imap(imap_account):
     if imap_account["useSSL"] == "true":
         client = imaplib.IMAP4_SSL(imap_account["host"], int(imap_account["port"]))
     else:
@@ -17,6 +21,19 @@ def check_unread(imap_account):
     client.login(imap_account["login"], imap_account["password"])
     client.select()
     return len(client.search(None, 'UNSEEN')[1][0].split())
+
+
+def check_gmail(gmail_account):
+    SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+    credential_file = 'gmail/' + gmail_account + '.json'
+    store = file.Storage(credential_file)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets('gmail/client_secret.json', SCOPES)
+        credentials = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=credentials.authorize(Http()))
+    results = service.users().messages().list(userId='me', q="is:unread").execute()
+    return len(results["messages"])
 
 
 for account in accounts:
@@ -27,6 +44,9 @@ for account in accounts:
         icon = accounts["DEFAULT"]["icon"]
     else:
         icon = currentAccount["icon"]
-    unread = check_unread(currentAccount)
+    if currentAccount['protocol'] == "GmailAPI":
+        unread = check_gmail(account)
+    else:
+        unread = check_imap(currentAccount)
     strFormatted += icon + " " + str(unread) + " "
 print(strFormatted)
